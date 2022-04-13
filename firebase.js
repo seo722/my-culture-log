7aef4cd43ad38d45320fcfda81c89d25e7134e03
seo722 <95078290+seo722@users.noreply.github.com>
2022-04-14 02:11:25 +0900

    next auth

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore();
 const storage = getStorage();
-const auth = getAuth();
 
 export default app;
-export { db, storage, auth };
+export { db, storage };
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+import { getProviders, getSession, useSession } from "next-auth/react";
 import Head from "next/head";
 import Image from "next/image";
+import Login from "../components/Login";
 
-export default function Home() {
+export default function Home({ providers }) {
+  const { data: session } = useSession();
+  console.log(session);
+
+  if (!session) return <Login providers={providers} />;
+
   return (
     <div>
       <div>This page gonna be my culture log</div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return { props: { session, providers } };
 }
